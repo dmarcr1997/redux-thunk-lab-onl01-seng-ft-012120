1cacbb21814235fcfd1843cefc68846a5cb30703
Damon R <dmarcr1997@gmail.com>
2020-06-04 18:51:39 +0000

    Automatically backed up by Learn

--- src/CatList.js
+++ src/CatList.js
@@ -5,7 +5,7 @@ class CatList extends Component{
   render(){
     return(
       <div>
-        {this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={kat.id} />)}
+        {this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)}
       </div>
     )
   }
