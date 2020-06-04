1c39c176b2d69fdff560a4d6f129e7f37153f58b
Damon R <dmarcr1997@gmail.com>
2020-06-04 18:45:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
- 
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import catsReducer from './reducers/catsReducer.js';
- 
+
 const store = createStore(catsReducer)
- 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
@@ -20,14 +20,14 @@ This is the Redux configuration we've seen previously. To implement Thunk, we'll
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
- 
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer.js';
- 
+
 const store = createStore(catsReducer, applyMiddleware(thunk))
- 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
